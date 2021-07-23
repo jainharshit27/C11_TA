f0577cddbd17ecf947d20c688820c3c1fa8c0e2d
jainharshit27 <82932932+jainharshit27@users.noreply.github.com>
2021-07-23 13:56:58 +0530

    Update TA1.py

--- TA1.py
+++ TA1.py
@@ -2,13 +2,19 @@ import pygame, pymunk
 import pymunk.pygame_util
 
 pygame.init()
-screen = pygame.display.set_mode((690, 600))
+
+height = 600
+width = 690
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 
 #pymunk space
+gravity = 1000
+wind = 200
 space = pymunk.Space()
-space.gravity = 0, 1000
+space.gravity = wind, gravity
 draw_options = pymunk.pygame_util.DrawOptions(screen)
+
 
 while True:
     screen.fill((255,255,255))
@@ -17,11 +23,6 @@ while True:
             pygame.quit()
             sys.exit()
 
-   
-    
-    space.debug_draw(draw_options)
-    
-    #space reload
-    space.step(1/60)
     pygame.display.update()
+    
     clock.tick(60)
